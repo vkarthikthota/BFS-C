5524ec59e61d25a121bb444d919dd6bf6f14fddd
vkarthikthota <vkarthikthota@users.noreply.github.com>
2014-09-04 14:47:55 -0700

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,40 @@
+
+
+#define _GRAPH_H_INCLUDE
+#ifndef _GRAPH_H_INCLUDE_
+
+#define NIL 0
+#define W 0
+#define INF -1
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+
+#include "List.h"
+
+typedef struct GraphObj* Graph;
+
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+
+/*** Access functions ***/ 
+int getOrder(Graph G); 
+int getSize(Graph G); 
+int getSource(Graph G); 
+int getParent(Graph G, int u); 
+int getDist(Graph G, int u); 
+void getPath(List L, Graph G, int u); 
+ 
+/*** Manipulation procedures ***/ 
+void makeNull(Graph G); 
+void addEdge(Graph G, int u, int v); 
+void addArc(Graph G, int u, int v); 
+void BFS(Graph G, int s); 
+ 
+ 
+/*** Other operations ***/ 
+void printGraph(FILE* out, Graph G); 
+
+#endif
